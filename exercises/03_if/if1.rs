9a0e5658fe5f3f0cf370438120b8d4d7dd659c7b
Aholu Samuel <samuelaholu15@gmail.com>
2025-10-23 19:48:22 +0100

    chore: excercise 3 progress

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,18 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    println!("19 and 8:");
+    println!("{} is a bigger number", bigger(19, 8));
+
 }
 
 // Don't mind this for now :)
--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -2,13 +2,17 @@
 fn picky_eater(food: &str) -> &str {
     if food == "strawberry" {
         "Yummy!"
-    } else {
-        1
+    } else if food == "potato" {
+            "I guess I can eat that."
+        } else {
+            "No thanks!"
+        }
     }
-}
+
 
 fn main() {
-    // You can optionally experiment here.
+    println!("{}", picky_eater("broccoli"));    
+    println!("{}", picky_eater("potatoe"));    
 }
 
 // TODO: Read the tests to understand the desired behavior.
